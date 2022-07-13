dac990c70499baf6e9578a5e683ad73bb9da8c5e
Ded12 <dedearifin1991@gmail.com>
2022-07-13 16:41:42 +0700

    Flutter contoh login dan index html

---
+++ lib/layers/layer_one.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+import 'package:login/config.dart';
+
+class LayerOne extends StatelessWidget {
+
+  const LayerOne({Key? key}) : super(key: key);
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 654,
+      decoration: const BoxDecoration(
+        color: layerOneBg,
+        borderRadius:  BorderRadius.only(
+          topLeft: Radius.circular(60.0),
+          bottomRight: Radius.circular(60.0)
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:login/layer_two.dart';
+import 'package:login/layers/layer_one.dart';
+import 'package:login/layers/layer_three.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "LoginPage",
+    theme: ThemeData(
+      fontFamily: "Poppins",
+    ),
+    debugShowCheckedModeBanner: false,
+    home: LoginPage(),
+  ));
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+          image: AssetImage('images/primaryBg.png'),
+          fit: BoxFit.cover
+    )),
+      child: Stack(
+        children: <Widget> [
+          Positioned(
+              top: 200,
+              left: 59,
+              child: Container(
+                child: const Text(
+                    'Login',
+                     style: TextStyle(
+                        fontSize: 48,
+                        fontFamily: 'Poppins-Medium',
+                        fontWeight: FontWeight.w500,
+                        color: Color.fromARGB(255, 0, 0, 0)),
+                )
+                )),
+            const Positioned(top: 290, right: 0, bottom: 0, child: LayerOne()),
+            Positioned(top: 318, right: 0, bottom: 28, child: LayerTwo()),
+            Positioned(top: 320, right: 0, bottom: 48, child: LayerThree()),
+          ], 
+        ),
+      ),
+    );
+  }
+}
